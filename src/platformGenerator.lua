5ce943f2e60387dc051473b454095f4f4aef8208
Lucas Caldeira <lucas.it08@gmail.com>
2017-12-22 17:22:21 -0200

    Platform generator now keeps track of every generated platform.

--- src/platformGenerator.lua
+++ src/platformGenerator.lua
@@ -1,10 +1,12 @@
 platformGenerator = {}
+platformGenerator.platforms = {}
 newPlatform = require ("src.platform")
 
 
 function platformGenerator:makeNewPlatform (platformX, platformY)
 	a = {}
 	a = newPlatform:new (a, platformX, platformY)
+	table.insert(platformGenerator.platforms, a)
 	return a
 end
 
