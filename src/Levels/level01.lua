22d93e123a856a12343089ca30ed6e830aac6a73
Lucas Caldeira <lucas.it08@gmail.com>
2017-12-27 01:20:34 -0200

    More progress in building level 1.

--- src/Levels/level01.lua
+++ src/Levels/level01.lua
@@ -4,7 +4,7 @@ wall = require ("src.wall")
 
 level01 = {}
 
-level01.platformCoordList = {{0, 250}, {300, 250}, {800, 350}, {1200, 350}, {1500, 300}, {2100, 300, 200}}
+level01.platformCoordList = {{0, 250}, {300, 250}, {800, 350}, {1200, 350}, {1500, 300}, {2100, 300, 200}, {2500, 300, 100}, {2850, 300, 1000}}
 level01.wallCoordList = {} 
 
 return level01
