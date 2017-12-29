968a70dade9ae838085d96952700d0e5f207ec44
Moskoman <lucas.it08@gmail.com>
2017-12-29 18:42:51 -0200

    Few level design changes in level 01.

--- src/Levels/level01.lua
+++ src/Levels/level01.lua
@@ -4,8 +4,8 @@ wall = require ("src.wall")
 
 level01 = {}
 
-level01.platformCoordList = {{0, 250}, {300, 250}, {800, 350}, {1200, 350}, {1500, 300}, {2100, 300, 200}, {2500, 300, 100}, {2850, 300, 1000}}
+level01.platformCoordList = {{0, 250}, {300, 250}, {800, 350}, {1200, 350}, {1500, 350, 300}, {1800, 300, 200}, {2100, 300, 200}, {2480, 300, 100}, {2840, 300, 1000}}
 level01.wallCoordList = {} 
-level01.powerStarList = {{850, 190}}
+level01.powerStarList = {{850, 190}, {1400, 250}}
 
 return level01
