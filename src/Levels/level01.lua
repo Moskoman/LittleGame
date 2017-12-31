3dc6b8a86062a44e25daff761d68430006e8c0f9
Moskoman <lucas.it08@gmail.com>
2017-12-31 10:57:51 -0200

    More level 01 changes, some PowerStars added and fixed flag position.

--- src/Levels/level01.lua
+++ src/Levels/level01.lua
@@ -6,8 +6,8 @@ level01 = {}
 
 level01.platformCoordList = {{0, 250}, {300, 250}, {800, 350}, {1200, 350}, {1500, 350, 300}, {1800, 300, 200}, {2100, 300, 200}, {2480, 300, 100}, {2840, 300, 1000}}
 level01.wallCoordList = {} 
-level01.powerStarList = {{850, 190}, {1400, 250}}
-level01.checkPointFlags = {{3000, 300}}
+level01.powerStarList = {{830, 190}, {1400, 250,}, {2400, 200}, {2620, 250}, {2720, 200}, {2820, 250}}
+level01.checkPointFlags = {{3000, 268}}
 
 
 return level01
