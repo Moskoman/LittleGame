cef5321b61c6d22adc08dde095707e7d19cd7d7b
Moskoman <lucas.it08@gmail.com>
2018-01-02 09:52:08 -0200

    Few more design stuff on level 2. Some walls and some platforms. Probably gonna change their position again in the future.

--- src/Levels/level02.lua
+++ src/Levels/level02.lua
@@ -4,9 +4,9 @@ wall = require ("src.wall")
 
 level02 = {}
 
-level02.platformCoordList = {{0, 300, 600}, {700, 300, 300}, {1150, 300, 100}, {1450, 340, 1000}}
-level02.wallCoordList = {{1800, 280, 40, 60}} 
+level02.platformCoordList = {{0, 300, 600}, {700, 300, 300}, {1150, 300, 100}, {1450, 340, 1000}, {2450, 300, 300}, {2600, 240, 500}, {2850, 130, 1000}}
+level02.wallCoordList = {{1800, 280, 40, 60}, {2100, 100, 40, 300}, {0, 0, 0, 0}} 
 level02.powerStarList = {{1870, 330}, {1900, 330}, {1930, 330}}
-level02.checkPointFlags = {{3000, 230}}
+level02.checkPointFlags = {}
 
 return level02
