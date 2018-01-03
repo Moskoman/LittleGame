1dffb627eb2ffcaaf1a9e175c359c39d87d63eaa
Moskoman <lucas.it08@gmail.com>
2018-01-03 10:54:43 -0200

    Started End Level Menu Pop-Up.

---
+++ src/endLevelMenu.lua
@@ -0,0 +1,17 @@
+levelManager = require ("src.levelManager")
+endLevelMenu = {}
+
+function endLevelMenu:New ()
+	
+end
+
+function endlevelMenu:Update ()
+	self:CheckInput ()
+
+end
+
+function endLevelMenu:CheckInput ()
+	
+end
+
+return endLevelMenu
