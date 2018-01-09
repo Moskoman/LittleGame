7e559658ce4d3a0f370afe5763bafc671281ba3d
Moskoman <lucas.it08@gmail.com>
2018-01-09 17:00:18 -0200

    Some changes in Camera and Player CheckDeath function.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public int moveSpeed = 30;
-    public float jumpForce = 20;
+    public float jumpForce = 30;
     public float dashForce = 20;
     public bool isDashing = false;
+    public bool isAlive = true;
     private bool canDash = true;
     private float dashCoolDown = 2f;
     private float dashCoolDownTimeStamp = 0;
     private Vector3 jumpForceVector;
+    private Camera camera;
     private Vector3 dashForceVector;
     
     public Rigidbody playerRigidbody;
@@ -23,14 +25,19 @@ public class Player : MonoBehaviour {
         playerRigidbody = GetComponent<Rigidbody>();
         jumpForceVector = new Vector3(0, jumpForce, 0);
         dashForceVector = new Vector3(dashForce, 0, 0);
+        camera = GetComponent<Camera>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        CheckCoolDown();
-        CheckInput();
-        Move();
+        CheckDeath();
+        if (isAlive)
+        {
+            CheckCoolDown();
+            CheckInput();
+            Move();
+        }
 	}
 
     private void Move () {
@@ -83,6 +90,14 @@ public class Player : MonoBehaviour {
         if (dashCoolDownTimeStamp + dashCoolDown <= Time.time)
         {
             canDash = true;
+        }
+    }
+
+    private void CheckDeath ()
+    {
+        if (transform.position.x < camera.transform.position.x)
+        {
+            isAlive = false;
         }
     }
 }
